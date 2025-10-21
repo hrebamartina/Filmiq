f802d871b13da2dbe0cd8bbda66593fa87ad3434
Martina <martingreba@gmail.com>
2025-10-21 21:39:06 +0300

    cganges

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,11 @@ import "./App.scss";
 import Header from "./components/Header/Header";
 
 function App() {
-  const test = 1 // забули крапку з комою
-  return (
+  return(
     <>
       <Header />
     </>
   );
 }
 
-
-
 export default App;
-
