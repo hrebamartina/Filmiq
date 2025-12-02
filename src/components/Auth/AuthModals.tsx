1aaf2d8e8e143d2bc33c304abc0a4da08d034b87
Martina Hreba <hreba.martina@student.uzhnu.edu.ua>
2025-12-02 13:45:05 +0200

    commit3

--- src/components/Auth/AuthModals.tsx
+++ src/components/Auth/AuthModals.tsx
@@ -1,24 +1,19 @@
-import { useAuthModalStore } from '../../store/authModalStore';
-import Modal from '../UI/Modal/Modal';
-import LoginForm from './LoginForm';
-import SignupForm from './SignupForm';
+import { useAuthModalStore } from "../../store/authModalStore";
+import Modal from "../UI/Modal/Modal";
+import LoginForm from "./LoginForm";
+import SignupForm from "./SignupForm";
 
 export default function AuthModals() {
+  const { modalView, closeModal, switchToLogin, switchToSignup } =
+    useAuthModalStore();
 
-  const { modalView, closeModal, switchToLogin, switchToSignup } = useAuthModalStore();
-
-  const isOpen = modalView !== 'closed';
+  const isOpen = modalView !== "closed";
 
   return (
     <Modal open={isOpen} onClose={closeModal}>
+      {modalView === "login" && <LoginForm onSwitchToSignup={switchToSignup} />}
 
-      {modalView === 'login' && (
-        <LoginForm onSwitchToSignup={switchToSignup} />
-      )}
-
-      {modalView === 'signup' && (
-        <SignupForm onSwitchToLogin={switchToLogin} />
-      )}
+      {modalView === "signup" && <SignupForm onSwitchToLogin={switchToLogin} />}
     </Modal>
-  )
+  );
 }
